9131a0a25f729692ec1aaf421f8260959b02e0df
Veera Jaswanth Reddy <71965179+jaswanth115@users.noreply.github.com>
2022-04-20 21:13:38 +0530

    Add files via upload

---
+++ html-home-task/index.js
@@ -0,0 +1,20 @@
+
+function tagchange(){
+    var b=" News";
+    var x=document.getElementById("select").value;
+    console.log(b);
+    document.getElementById("tag").innerHTML = x+b; 
+     
+}
+function date(){
+    const d = new Date();
+    document.getElementById("date1").innerHTML = d;
+    document.getElementById("date2").innerHTML = d;
+    document.getElementById("date3").innerHTML = d;
+}
+function paragraph(){
+    const d = "Delhi is a crowded city. There are very few rich people who travel by their own vehicles.";
+    document.getElementById("para1").innerHTML = d;
+    document.getElementById("para2").innerHTML = d;
+    document.getElementById("para3").innerHTML = d;
+}
