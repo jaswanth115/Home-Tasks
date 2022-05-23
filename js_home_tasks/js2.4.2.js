7e857af686bbbaa891aa57cbfff67725ecedcb98
Veera Jaswanth Reddy <71965179+jaswanth115@users.noreply.github.com>
2022-05-23 11:53:42 +0530

    Delete js_home_tasks directory

--- js_home_tasks/js2.4.2.js
+++
@@ -1,9 +0,0 @@
-//We want to create a function that will add numbers together when called in succession.
-//We also want to be able to continue to add numbers to our chain.
-
-function add(n){
-    // Let the currying begin!
-    const f = x => add(n + x);
-    f.valueOf = () => n;
-    return f;
-  }
